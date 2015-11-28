d01860b678161c87fc06e0bf6e17f79463b0cea9
james <acejames@msn.com>
2015-11-28 20:56:21 +0000

    first

---
+++ weather/src/DelimitedReader.py
@@ -0,0 +1,29 @@
+import csv
+
+"""Reads delimeted files"""
+class DelimitedReader:
+
+    
+    def __init__(self):
+        pass
+    
+    def read(self, filename,func):
+        with open(filename,'rb')as tsvin:
+            tsvin = csv.reader(tsvin,delimiter='\t')
+            
+#            lastyear = None
+            for row in tsvin:
+                func(row)
+#                if row[0]!=lastyear:
+#                    lastyear = row[0]
+#                    print lastyear
+#                else:
+#                    print "hat"
+                #===============================================================
+                # func(row)
+                #===============================================================
+                
+
+        
+
+            
---
+++ weather/src/m.py
@@ -0,0 +1,15 @@
+'''
+Created on 21 Aug 2015
+
+@author: James
+'''
+from HottestDay import HottestDay
+
+if __name__ == '__main__':
+    hottestDay = HottestDay()
+    years = hottestDay.assembleYears("../resources/weather.tsv")
+    
+    print years
+    hottestDay.retainHottestDays(years)
+    
+        
